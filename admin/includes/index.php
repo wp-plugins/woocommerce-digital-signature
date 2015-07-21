7228b19acf7b0293a167af1d55343de87f9c77ef
approveme <approveme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-21 10:08:29 +0000

    1.2.2 released
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-digital-signature/trunk@1203003 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/includes/index.php
+++
@@ -1,4 +0,0 @@
-<?php // Silence is golden
-if ( ! defined( 'ABSPATH' ) ) { 
-	exit; // Exit if accessed directly
-}
--- admin/views/esig-not-core-view.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Silence is golden
-if ( ! defined( 'ABSPATH' ) ) { 
-	exit; // Exit if accessed directly
-}
-?>
-  esignature not install will be here
